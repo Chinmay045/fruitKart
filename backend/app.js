c3ba72c8e65816962f865b555e920a3a1f38d196
Chinmay <chinz.sandur07@gmail.com>
2024-03-04 09:41:33 +0530

    backend is setup

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const cors = require('cors');
+const productsRouter = require('./routes/productsRoute.js');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(productsRouter);
+
 
 app.listen(3300, () => {
     console.log("Server Started at port 3300");
--- backend/routes/productsRoute.js
+++ backend/routes/productsRoute.js
@@ -1,3 +1,43 @@
 const express = require('express');
 const router = express.Router();
 
+const data = {
+    productItems: [
+        {
+            id: "1",
+            name: "Apple Fresh",
+            price: 499,
+            image: ""
+        },
+        {
+            id: "2",
+            name: "Banana",
+            price: 99,
+            image: ""
+        },
+        {
+            id: "3",
+            name: "Maango",
+            price: 299,
+            image: ""
+        },
+        {
+            id: "4",
+            name: "Kiwi",
+            price: 699,
+            image: ""
+        },
+        {
+            id: "5",
+            name: "Pinapple",
+            price: 199,
+            image: ""
+        }
+    ]
+}
+
+router.get('/products', (req, res) => {
+    res.json(data);
+});
+
+module.exports = router;
